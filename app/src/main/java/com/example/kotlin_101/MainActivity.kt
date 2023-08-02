e1b8a57fed0f5df0b9992a065266731b91c91836
iremerdgn9 <irmerdgn06@gmail.com>
2023-08-02 22:57:07 +0300

    initial commit

---
+++ app/src/main/java/com/example/kotlin_101/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_login_ui)
+    }
+}
