9b488360278623b76b2605b86d36f869c6f5fd82
iremerdgn9 <irmerdgn06@gmail.com>
2023-08-18 20:03:09 +0300

    Toast ve touch listener özelliği ile buton uyarı mesajı gösterme yapıldı

--- app/src/main/java/com/example/kotlin_101/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_101/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.kotlin_101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_login_ui)
+
+
+        window.decorView.rootView.setOnTouchListener(View.OnTouchListener{view, motionEvent ->
+            Toast.makeText(this, "Ekrana Dokunuldu", Toast.LENGTH_SHORT).show()
+            return@OnTouchListener true
+        })
+
     }
+    fun button2(view: View) {
+       Toast.makeText(this, "Giriş yapıldı", Toast.LENGTH_LONG).show()
+    }
+
+
 }
